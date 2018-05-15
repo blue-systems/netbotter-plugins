df6bc2e9b2f395f740f0148caf73ba3d9981abfb
Harald Sitter <sitter@kde.org>
2018-05-15 16:43:58 +0200

    implement chair functionality
    only the chair may call next
    
    by default the person initiating the roll call is chair.
    others may take the chair via `take chair` or `give chair to nick`

--- meeting.rb
+++ meeting.rb
@@ -10,7 +10,8 @@ class MeetingPlugin < Plugin
 roll call => highlight everyone and the kitchen sink, records anyone saying anything as present
 next => move to next person in queue
 move :name up => moves the nick :name to the front of the queue
-All commands have numerous aliases.
+take chair => take the chair for yourself. gives you `next` powers. there may only be one!
+give chair to :name => give the chair to someone else
     HELP
   end
 
@@ -22,23 +23,24 @@ All commands have numerous aliases.
   end
 
   def roll_call(m, **_)
+    @chair = m.sourcenick
+    @pending = [@chair] # Chair is first in array, but last overall.
     nicks = m.channel.users.collect(&:nick)
     nicks.reject! { |n| n == bot.nick }
-    m.reply("Roll Call! #{nicks.join(', ')}")
-    @pending = [m.sourcenick] # Chair is first in array, but last overall.
+    m.reply "Roll Call! #{nicks.join(', ')}"
     @active = true
   end
 
+  def take_chair(m, name: nil, **_)
+    return if abort_active?(m)
+    @chair = name || m.sourcenick
+    m.reply "The Right Honourable #{@chair} is now presiding."
+  end
+
   def next(m, **_)
-    unless @active
-      m.reply 'No meeting in progress. Either do a roll call, or we are done!'
-      return
-    end
+    return if abort_active?(m)
+    return if abort_chair?(m)
     @current = @pending.shift
-    # unless @current
-    #   m.reply 'Out of people to harass :( - End by telling me: meeting done'
-    #   return
-    # end
     m.reply "Next up #{@current} (followed by #{@pending.fetch(0, 'silence!')})"
     @done << @current
 
@@ -50,6 +52,7 @@ All commands have numerous aliases.
     p @done
     p @active
     p @current
+    p ['chair', @chair]
   end
 
   def done(_, **_)
@@ -57,10 +60,7 @@ All commands have numerous aliases.
   end
 
   def move_up(m, name:, **_)
-    unless @active
-      m.reply 'No meeting in progress. Do a roll call first?'
-      return
-    end
+    return if abort_active?(m)
     nick = @pending.delete(name)
     unless nick
       m.reply "#{name} not in pending stack"
@@ -70,13 +70,39 @@ All commands have numerous aliases.
     m.reply "Moved #{nick} to the front of the queue."
   end
 
+  def move_back(m, name:, **_)
+    return if abort_active?(m)
+    return if @pending.empty?
+    nick = @pending.delete(name)
+    @pending.insert(-2, name)
+    m.reply "Moved #{nick} to the back of the queue."
+  end
+
   private
+
+  def abort_chair?(m)
+    unless m.sourcenick == @chair
+      m.reply "To avoid confusion, only the chair (#{@chair}) may progress." \
+              ' Or you may take the chair with `take chair`.'
+      return true
+    end
+    false
+  end
+
+  def abort_active?(m)
+    unless @active
+      m.reply 'No meeting in progress. Either do a roll call, or we are done!'
+      return true
+    end
+    false
+  end
 
   def reset!
     @pending = []
     @done = []
     @active = false
     @current = nil
+    @chair = nil
   end
 end
 
@@ -100,3 +126,5 @@ plugin.map 'start', action: 'next'
 plugin.map 'meeting start', action: 'next'
 plugin.map 'move :name up', action: 'move_up'
 # plugin.map 'move :name back', action: 'move_back'
+plugin.map 'take chair', action: 'take_chair'
+plugin.map 'give chair to :name', action: 'take_chair'
